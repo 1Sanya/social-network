c26204e8817fe2817a1428c1086232c7287afe0a
Sanya <sanyan1of@gmail.com>
2021-05-26 19:48:29 +0300

    ff roboto, nav style

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import './Nav.module.css'
 import s from './Nav.module.css'
 import NavLi from './navLi/NavLi'
-import {CgProfile} from "react-icons/all";
+import {CgProfile} from 'react-icons/cg';
+import {FiMessageCircle} from 'react-icons/fi'
+import {IoPeopleOutline} from 'react-icons/all'
+import {HiOutlinePhotograph} from 'react-icons/all'
 
 
 let Nav = ()=> {
     return (
         <div className={s.nav_wr}>
 
-           <div className={s.single_li}> <CgProfile/> <div className={s.li_text}>  Profile</div></div>
-            <NavLi text='Messages'/>
-            <NavLi text='Friends'/>
-            <NavLi text='Photos'/>
+           <div className={s.single_li}> <CgProfile color='#586069' size='1.2em'/> <div className={s.li_text}>My profile</div></div>
+           <div className={s.single_li}> <FiMessageCircle color='#586069' size='1.2em'/> <div className={s.li_text}>Messenger</div></div>
+           <div className={s.single_li}> <IoPeopleOutline color='#586069' size='1.2em'/> <div className={s.li_text}>Friends</div></div>
+           <div className={s.single_li}> <HiOutlinePhotograph color='#586069' size='1.2em'/> <div className={s.li_text}>Photos</div></div>
+
         </div>
     )
 };
