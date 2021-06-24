67882583bac3e92d602a86d40dcb54c46bf5ba61
Sanya <sanyan1of@gmail.com>
2021-06-24 16:18:27 +0300

    SamuraiJS Social Network API connected, friends.users now gets from api

--- src/Components/Friends/Friends.jsx
+++ src/Components/Friends/Friends.jsx
@@ -5,18 +5,16 @@ import Friends_header from "./Friends_header/Friends_header";
 
 import May_be_friends from "./May_be_friends/May_be_friends";
 import Peoples_category from "./Peoples_category/Peoples_category";
+import * as axios from 'axios'
 
 import s from './Friends.module.css'
 
 let Friends = (props) => {
 
     if(props.profiles.length === 0) {
-        props.setUsers([
-            {id: 1, name: 'Sanya', isFollow: true, pic: 'https://svirtus.cdnvideo.ru/TvRTmt9sgudd3kSzLsTE_HeqZus=/0x0:583x585/800x0/filters:quality(100)/https://hb.bizmrg.com/cybersportru-media/66/661cc81674bd7cd63f56bdc2826fb8b3.png?m=cf0455e62a7aa51de43f6a1947ff0198'},
-            {id: 2, name: 'Danya', isFollow: true, pic: 'https://svirtus.cdnvideo.ru/TvRTmt9sgudd3kSzLsTE_HeqZus=/0x0:583x585/800x0/filters:quality(100)/https://hb.bizmrg.com/cybersportru-media/66/661cc81674bd7cd63f56bdc2826fb8b3.png?m=cf0455e62a7aa51de43f6a1947ff0198'},
-            {id: 3, name: 'Dima', isFollow: true, pic: 'https://svirtus.cdnvideo.ru/TvRTmt9sgudd3kSzLsTE_HeqZus=/0x0:583x585/800x0/filters:quality(100)/https://hb.bizmrg.com/cybersportru-media/66/661cc81674bd7cd63f56bdc2826fb8b3.png?m=cf0455e62a7aa51de43f6a1947ff0198'},
-            {id: 4, name: 'Nasty', isFollow: true, pic: 'https://svirtus.cdnvideo.ru/TvRTmt9sgudd3kSzLsTE_HeqZus=/0x0:583x585/800x0/filters:quality(100)/https://hb.bizmrg.com/cybersportru-media/66/661cc81674bd7cd63f56bdc2826fb8b3.png?m=cf0455e62a7aa51de43f6a1947ff0198'}
-        ])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        })
     }
 
     return (
