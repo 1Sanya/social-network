8dc9df63d197a1a8a85cbaa4df8e95744be90ef3
1Sanya <sanyan1of@gmail.com>
2021-11-04 05:25:56 +0300

    delete messenger trompley

--- src/Components/DialogsTrompley/DialogsTrompley.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react'
-import s from './DialogsTrompley.module.css'
-
-export const DialogsTrompley: FC = () => (
-  <div className={s.page_wr} />
-)
-
-export default DialogsTrompley
